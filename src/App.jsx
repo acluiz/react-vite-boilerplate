235485fbca44925904eddba4b8324567f45e4d84
Ana Luiz <anamurdock881@gmail.com>
2023-05-02 19:30:11 -0300

    chore: add prettier/eslint

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-import '@/styles/index.scss';
+import "@/styles/index.scss"
 
 function App() {
-  return (
-    <main id="main">
-      <h1>Hello, World!</h1>
-    </main>
-  )
+	return (
+		<main id="main">
+			<h1>Hello, World!</h1>
+		</main>
+	)
 }
 
 export default App
